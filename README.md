e9a0cb869fb2b7f96a6457c02560273bf1b9b42f
Minh Truong <minhtruongdevvn@gmail.com>
2023-05-27 11:42:02 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,121 @@
+# My Book - Facebook functional clone app (back-end)
+
+## Table of Contents
+
+- [My Book - Facebook functional clone app (back-end)](#my-book---facebook-functional-clone-app-back-end)
+  - [Table of Contents](#table-of-contents)
+  - [Quick run](#quick-run)
+  - [Comfortable development](#comfortable-development)
+  - [Links](#links)
+  - [Automatic update of dependencies](#automatic-update-of-dependencies)
+  - [Database utils](#database-utils)
+  - [Tests](#tests)
+  - [Tests in Docker](#tests-in-docker)
+  - [Test benchmarking](#test-benchmarking)
+
+## Quick run
+
+```bash
+git clone --depth 1 https://github.com/brocoders/nestjs-boilerplate.git my-app
+cd my-app/
+cp env-example .env
+docker compose up -d
+```
+
+For check status run
+
+```bash
+docker compose logs
+```
+
+## Comfortable development
+
+```bash
+git clone --depth 1 https://github.com/brocoders/nestjs-boilerplate.git my-app
+cd my-app/
+cp env-example .env
+```
+
+Change `DATABASE_HOST=postgres` to `DATABASE_HOST=localhost`
+
+Change `MAIL_HOST=maildev` to `MAIL_HOST=localhost`
+
+Run additional container:
+
+```bash
+docker compose up -d postgres adminer maildev
+```
+
+```bash
+npm install
+
+npm run migration:run
+
+npm run seed:run
+
+npm run start:dev
+```
+
+## Links
+
+- Swagger: http://localhost:3000/docs
+- Adminer (client for DB): http://localhost:8080
+- Maildev: http://localhost:1080
+
+## Automatic update of dependencies
+
+Todo: connect [Renovate](https://github.com/marketplace/renovate) for the project.
+
+## Database utils
+
+Generate migration
+
+```bash
+npm run migration:generate -- src/database/migrations/CreateNameTable
+```
+
+Run migration
+
+```bash
+npm run migration:run
+```
+
+Revert migration
+
+```bash
+npm run migration:revert
+```
+
+Drop all tables in database
+
+```bash
+npm run schema:drop
+```
+
+Run seed
+
+```bash
+npm run seed:run
+```
+
+## Tests
+
+```bash
+# unit tests
+npm run test
+
+# e2e tests
+npm run test:e2e
+```
+
+## Tests in Docker
+
+```bash
+docker compose -f docker-compose.ci.yaml --env-file env-example -p ci up --build --exit-code-from api && docker compose -p ci rm -svf
+```
+
+## Test benchmarking
+
+```bash
+docker run --rm jordi/ab -n 100 -c 100 -T application/json -H "Authorization: Bearer USER_TOKEN" -v 2 http://<server_ip>:3000/api/v1/users
+```
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,120 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Patch,
+  Post,
+  Request,
+  SerializeOptions,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { User } from '../users/entities/user.entity';
+import { LoginResponseType } from '../utils/types/auth/login-response.type';
+import { NullableType } from '../utils/types/nullable.type';
+import { AuthService } from './auth.service';
+import { AuthConfirmEmailDto } from './dto/auth-confirm-email.dto';
+import { AuthEmailLoginDto } from './dto/auth-email-login.dto';
+import { AuthForgotPasswordDto } from './dto/auth-forgot-password.dto';
+import { AuthRegisterLoginDto } from './dto/auth-register-login.dto';
+import { AuthResetPasswordDto } from './dto/auth-reset-password.dto';
+import { AuthUpdateDto } from './dto/auth-update.dto';
+
+@ApiTags('Auth')
+@Controller({
+  path: 'auth',
+  version: '1',
+})
+export class AuthController {
+  constructor(private readonly service: AuthService) {}
+
+  @SerializeOptions({
+    groups: ['me'],
+  })
+  @Post('email/login')
+  @HttpCode(HttpStatus.OK)
+  public login(
+    @Body() loginDto: AuthEmailLoginDto,
+  ): Promise<LoginResponseType> {
+    return this.service.validateLogin(loginDto, false);
+  }
+
+  @SerializeOptions({
+    groups: ['me'],
+  })
+  @Post('admin/email/login')
+  @HttpCode(HttpStatus.OK)
+  public adminLogin(
+    @Body() loginDTO: AuthEmailLoginDto,
+  ): Promise<LoginResponseType> {
+    return this.service.validateLogin(loginDTO, true);
+  }
+
+  @Post('email/register')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async register(@Body() createUserDto: AuthRegisterLoginDto): Promise<void> {
+    return this.service.register(createUserDto);
+  }
+
+  @Post('email/confirm')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async confirmEmail(
+    @Body() confirmEmailDto: AuthConfirmEmailDto,
+  ): Promise<void> {
+    return this.service.confirmEmail(confirmEmailDto.hash);
+  }
+
+  @Post('forgot/password')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async forgotPassword(
+    @Body() forgotPasswordDto: AuthForgotPasswordDto,
+  ): Promise<void> {
+    return this.service.forgotPassword(forgotPasswordDto.email);
+  }
+
+  @Post('reset/password')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  resetPassword(@Body() resetPasswordDto: AuthResetPasswordDto): Promise<void> {
+    return this.service.resetPassword(
+      resetPasswordDto.hash,
+      resetPasswordDto.password,
+    );
+  }
+
+  @ApiBearerAuth()
+  @SerializeOptions({
+    groups: ['me'],
+  })
+  @Get('me')
+  @UseGuards(AuthGuard('jwt'))
+  @HttpCode(HttpStatus.OK)
+  public me(@Request() request): Promise<NullableType<User>> {
+    return this.service.me(request.user);
+  }
+
+  @ApiBearerAuth()
+  @SerializeOptions({
+    groups: ['me'],
+  })
+  @Patch('me')
+  @UseGuards(AuthGuard('jwt'))
+  @HttpCode(HttpStatus.OK)
+  public update(
+    @Request() request,
+    @Body() userDto: AuthUpdateDto,
+  ): Promise<NullableType<User>> {
+    return this.service.update(request.user, userDto);
+  }
+
+  @ApiBearerAuth()
+  @Delete('me')
+  @UseGuards(AuthGuard('jwt'))
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public async delete(@Request() request): Promise<void> {
+    return this.service.softDelete(request.user);
+  }
+}
